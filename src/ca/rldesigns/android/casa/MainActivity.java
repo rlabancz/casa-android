d95e4484e84a87e419f8d901125d55ed7c41292e
Robert Labancz <robert.labancz@gmail.com>
2014-05-14 23:19:17 -0400

    initial commit

---
+++ src/ca/rldesigns/android/casa/MainActivity.java
@@ -0,0 +1,154 @@
+package ca.rldesigns.android.casa;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity implements LocationListener {
+
+	private TextView txtLat;
+	private ImageView imageView1;
+	private RelativeLayout background;
+	LocationManager locationManager;
+
+	Drawable backgroundD;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+		}
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public class DownloadImagesTask extends AsyncTask<ImageView, Void, Bitmap> {
+
+		ImageView imageView = null;
+
+		@Override
+		protected Bitmap doInBackground(ImageView... imageViews) {
+			this.imageView = imageViews[0];
+			return download_Image((String) imageView.getTag());
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			imageView.setImageBitmap(result);
+		}
+
+		private Bitmap download_Image(String url) {
+			// ---------------------------------------------------
+			Bitmap bm = null;
+			try {
+				URL aURL = new URL(url);
+				URLConnection conn = aURL.openConnection();
+				conn.connect();
+				InputStream is = conn.getInputStream();
+				BufferedInputStream bis = new BufferedInputStream(is);
+				bm = BitmapFactory.decodeStream(bis);
+				bis.close();
+				is.close();
+			} catch (IOException e) {
+				Log.e("Hub", "Error getting the image from server : " + e.getMessage().toString());
+			}
+			return bm;
+			// ---------------------------------------------------
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+
+		txtLat = (TextView) findViewById(R.id.textview1);
+		txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+
+		background = (RelativeLayout) findViewById(R.id.background);
+		imageView1 = (ImageView) findViewById(R.id.imageView1);
+		String URL = "http://cdn.realtor.ca/listing/reb82/highres/3/c2855403_5.jpg";
+		imageView1.setTag(URL);
+		new DownloadImagesTask().execute(imageView1);
+
+		new SendDataAsync().execute(this, location.getLatitude(), location.getLongitude(), 2);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		Log.d("Latitude", "disable");
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		Log.d("Latitude", "enable");
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		Log.d("Latitude", "status");
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			return rootView;
+		}
+	}
+
+}
