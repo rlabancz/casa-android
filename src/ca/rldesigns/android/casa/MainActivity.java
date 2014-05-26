36a7dc0bb430e128788d1980ce2075dabe8e277f
Robert Labancz <Robert Labancz@mobileRob>
2014-05-26 11:29:02 -0400

    removed unused files

--- src/ca/rldesigns/android/casa/MainActivity.java
+++ src/ca/rldesigns/android/casa/MainActivity.java
@@ -1,11 +1,6 @@
 package ca.rldesigns.android.casa;
 
 import static ca.rldesigns.android.casa.ApplicationData.DATABASE_NAME;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Calendar;
 
 import org.json.JSONException;
@@ -26,11 +21,7 @@ import ca.rldesigns.android.casa.utils.ResultCodes;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -41,7 +32,6 @@ import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -49,8 +39,6 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements OnKeyListener, OnSeekBarChangeListener, OnRangeBarChangeListener, OnClickListener {
 
 	private SharedPreferences savedSettings;
-
-	private ImageView imageView1;
 
 	private Button setLocation;
 	private TextView location;
@@ -162,12 +150,6 @@ public class MainActivity extends Activity implements OnKeyListener, OnSeekBarCh
 		storiesMin.setText(Integer.toString(storiesMinValue));
 		storiesMax = (EditText) findViewById(R.id.stories_max);
 		storiesMax.setText(Integer.toString(storiesMaxValue));
-
-		imageView1 = (ImageView) findViewById(R.id.imageView1);
-		String URL = "http://cdn.realtor.ca/listing/reb82/highres/3/c2855403_5.jpg";
-		imageView1.setTag(URL);
-		// new DownloadImagesTask().execute(imageView1);
-		// new SendDataAsync().execute(this, location.getLatitude(), location.getLongitude(), 2);
 	}
 
 	@Override
@@ -351,41 +333,6 @@ public class MainActivity extends Activity implements OnKeyListener, OnSeekBarCh
 				selectedLatLng = ActionParams.SELECTED_POSITION;
 			}
 			break;
-		}
-	}
-
-	public class DownloadImagesTask extends AsyncTask<ImageView, Void, Bitmap> {
-
-		ImageView imageView = null;
-
-		@Override
-		protected Bitmap doInBackground(ImageView... imageViews) {
-			this.imageView = imageViews[0];
-			return download_Image((String) imageView.getTag());
-		}
-
-		@Override
-		protected void onPostExecute(Bitmap result) {
-			imageView.setImageBitmap(result);
-		}
-
-		private Bitmap download_Image(String url) {
-			// ---------------------------------------------------
-			Bitmap bm = null;
-			try {
-				URL aURL = new URL(url);
-				URLConnection conn = aURL.openConnection();
-				conn.connect();
-				InputStream is = conn.getInputStream();
-				BufferedInputStream bis = new BufferedInputStream(is);
-				bm = BitmapFactory.decodeStream(bis);
-				bis.close();
-				is.close();
-			} catch (IOException e) {
-				Log.e("Hub", "Error getting the image from server : " + e.getMessage().toString());
-			}
-			return bm;
-			// ---------------------------------------------------
 		}
 	}
 }
